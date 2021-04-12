609a6fe66c9b8ecb9fc173c1b140271a8805f845
H-J Yoo <yoohj@smu.ac.kr>
2021-04-12 19:43:04 +0900

    location providers' info

--- app/src/main/java/com/androidapp/locproviderinfo/MainActivity.java
+++ app/src/main/java/com/androidapp/locproviderinfo/MainActivity.java
@@ -50,19 +50,21 @@ public class MainActivity extends AppCompatActivity {
         txtInfo.append("이름: " + locationProvider.getName() + "\n");
         txtInfo.append("현재이용가능여부: " +
                 locationManager.isProviderEnabled(locationProvider.getName()) + "\n");
+
         txtInfo.append("위성필요여부: " + locationProvider.requiresSatellite() + "\n");
         txtInfo.append("인터넷접속필요여부: " + locationProvider.requiresNetwork() + "\n");
         txtInfo.append("기지국필요여부: " + locationProvider.requiresCell() + "\n");
 
-        txtInfo.append("고도정보포함여부: " + locationProvider.supportsAltitude() + "\n");
-        txtInfo.append("방향정보포함여부: " + locationProvider.supportsBearing() + "\n");
-        txtInfo.append("속도정보포함여부: " + locationProvider.supportsSpeed() + "\n");
+        txtInfo.append("고도정보지원여부: " + locationProvider.supportsAltitude() + "\n");
+        txtInfo.append("방향정보지원여부: " + locationProvider.supportsBearing() + "\n");
+        txtInfo.append("속도정보지원여부: " + locationProvider.supportsSpeed() + "\n");
+
         String accuracy = "";
         switch(locationProvider.getAccuracy()) {
             //2.3부터 사용
-            case Criteria.ACCURACY_LOW: accuracy="500m 이상 오차"; break;
+            case Criteria.ACCURACY_LOW:    accuracy="500m 이상 오차"; break;
             case Criteria.ACCURACY_MEDIUM: accuracy="100~500m 오차"; break;
-            case Criteria.ACCURACY_HIGH: accuracy="0~100m 오차"; break;
+            case Criteria.ACCURACY_HIGH:   accuracy="0~100m 오차"; break;
         }
         txtInfo.append("정확성정도: " + accuracy + "\n");
 
