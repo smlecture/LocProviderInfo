4449daddb0fcc99b43bd40c0071b30a568a08b30
H-J Yoo <yoohj@smu.ac.kr>
2021-04-12 19:25:36 +0900

    location providers' info

--- app/src/main/java/com/androidapp/locproviderinfo/MainActivity.java
+++ app/src/main/java/com/androidapp/locproviderinfo/MainActivity.java
@@ -2,7 +2,9 @@ package com.androidapp.locproviderinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Criteria;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     TextView txtInfo;
     String s;
     List<String> locProviderList;
+    LocationProvider locProvider; //, networkProvider, passiveProvider;
+    Criteria criteria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +29,53 @@ public class MainActivity extends AppCompatActivity {
 
         locProviderList = locationManager.getAllProviders();
         txtInfo.setText("");
-        for(String locProvider : locProviderList){
-            txtInfo.append("Loc. Provider: " + locProvider + "\n");
-            txtInfo.append("Enabled: " + locationManager.isProviderEnabled(locProvider) + "\n\n");
+        for(String locProviderName : locProviderList){
+            txtInfo.append("Loc. Provider: " + locProviderName + "\n");
+            txtInfo.append("Enabled: " + locationManager.isProviderEnabled(locProviderName) + "\n\n");
+            switch(locProviderName){
+                case "gps":
+                    locProvider = locationManager.getProvider(LocationManager. GPS_PROVIDER);
+                    break;
+                case "newtork":
+                    locProvider = locationManager.getProvider(LocationManager. NETWORK_PROVIDER);
+                    break;
+                case "passive":
+                    locProvider = locationManager.getProvider(LocationManager. NETWORK_PROVIDER);
+                    break;
+            }
+            printInfo(locProvider);
         }
+    }
+    private void printInfo(LocationProvider locationProvider) {
+        txtInfo.append("이름: " + locationProvider.getName() + "\n");
+        txtInfo.append("현재이용가능여부: " +
+                locationManager.isProviderEnabled(locationProvider.getName()) + "\n");
+        txtInfo.append("위성필요여부: " + locationProvider.requiresSatellite() + "\n");
+        txtInfo.append("인터넷접속필요여부: " + locationProvider.requiresNetwork() + "\n");
+        txtInfo.append("기지국필요여부: " + locationProvider.requiresCell() + "\n");
+
+        txtInfo.append("고도정보포함여부: " + locationProvider.supportsAltitude() + "\n");
+        txtInfo.append("방향정보포함여부: " + locationProvider.supportsBearing() + "\n");
+        txtInfo.append("속도정보포함여부: " + locationProvider.supportsSpeed() + "\n");
+        String accuracy = "";
+        switch(locationProvider.getAccuracy()) {
+            //2.3부터 사용
+            case Criteria.ACCURACY_LOW: accuracy="500m 이상 오차"; break;
+            case Criteria.ACCURACY_MEDIUM: accuracy="100~500m 오차"; break;
+            case Criteria.ACCURACY_HIGH: accuracy="0~100m 오차"; break;
+        }
+        txtInfo.append("정확성정도: " + accuracy + "\n");
+
+        String power = "";
+        switch(locationProvider.getPowerRequirement()) {
+            case Criteria.POWER_LOW: 	power="적게 사용"; break;
+            case Criteria.POWER_MEDIUM: power="중간 사용"; break;
+            case Criteria.POWER_HIGH: 	power="많이 사용"; break;
+        }
+        txtInfo.append("전원사용정도: " + power + "\n");
+
+        txtInfo.append("요금지불여부: " + locationProvider.hasMonetaryCost() + "\n");
+        txtInfo.append("\n");
+
     }
 }
